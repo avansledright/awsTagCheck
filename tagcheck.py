204006e47943bcf8e9ea4966b5d812d0c1d20610
Aaron VanSledright <avansledright@gmail.com>
2020-09-01 12:09:20 -0500

    First Commit

---
+++ tagcheck.py
@@ -0,0 +1,22 @@
+import boto3
+
+
+ec2 = boto3.resource('ec2')
+inst_describe = ec2.instances.all()
+
+for instance in inst_describe:
+    if 'Backup' not in [t['Key'] for t in instance.tags]:
+        print("This instance is not tagged: ", instance.instance_id)
+        response = ec2.create_tags(
+            Resources= [instance.instance_id],
+            Tags = [
+                {
+                    'Key': 'Backup',
+                    'Value': 'Yes'
+                }
+            ]
+        )
+# Double check that there are no other instances without tags
+for instance in inst_describe:
+    if 'Backup' not in [t['Key'] for t in instance.tags]:
+        print("Failed to assign tag, or new instance: ", instance.instance_id)        
